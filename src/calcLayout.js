1aa8eb8f0917c2194ef506481044656024adbe73
rhawkens99 <r.hawkens.d@gmail.com>
2021-10-16 19:42:18 -0700

    start of gui

---
+++ src/calcLayout.js
@@ -0,0 +1,41 @@
+const { QPushButton } = require("@nodegui/nodegui");
+
+// buttons
+const zero = new QPushButton();
+const one  = new QPushButton();
+const two = new QPushButton();
+const three = new QPushButton();
+const four = new QPushButton();
+const five  = new QPushButton();
+const six = new QPushButton();
+const seven = new QPushButton();
+const eight = new QPushButton();
+const nine = new QPushButton();
+
+const plus = new QPushButton();
+const subtract = new QPushButton();
+const times = new QPushButton();
+const divide = new QPushButton();
+const exponent = new QPushButton();
+
+const equals = new QPushButton();
+const clear = new QPushButton();
+const backspace = new QPushButton();
+
+const pi = new QPushButton();
+
+const sin = new QPushButton();
+const cos = new QPushButton();
+const tan = new QPushButton();
+const sec = new QPushButton();
+const csc = new QPushButton();
+const cot = new QPushButton();
+const arcsin = new QPushButton();
+const arccos = new QPushButton();
+const arctan = new QPushButton();
+const arcsec = new QPushButton();
+const arccsc = new QPushButton();
+const arcot = new QPushButton();
+
+const ln = new QPushButton();
+const log = new QPushButton();
